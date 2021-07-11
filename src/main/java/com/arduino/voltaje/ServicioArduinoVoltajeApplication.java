37ef86dc19dbb2abcc3f9ad30d54b822925093b7
sergycg <sergycg@gmail.com>
2021-07-11 17:52:10 +0200

    add heroku Not Idle

--- src/main/java/com/arduino/voltaje/ServicioArduinoVoltajeApplication.java
+++ src/main/java/com/arduino/voltaje/ServicioArduinoVoltajeApplication.java
@@ -7,9 +7,12 @@ import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableScheduling
 public class ServicioArduinoVoltajeApplication {
 
 //	@PostConstruct
@@ -21,4 +24,9 @@ public class ServicioArduinoVoltajeApplication {
 		SpringApplication.run(ServicioArduinoVoltajeApplication.class, args);
 	}
 
+    @Scheduled(fixedDelay = 120000, initialDelay = 500)
+    public void herokuNotIdle() {
+	System.out.println(" Heroku not idle execution ");
+
+    }
 }
