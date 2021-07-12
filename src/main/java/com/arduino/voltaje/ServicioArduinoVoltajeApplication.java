44d00ef24538f12314bf1e291772d5c10af37e1c
sergycg <sergycg@gmail.com>
2021-07-12 14:51:22 +0200

    add heroku Not Idle

--- src/main/java/com/arduino/voltaje/ServicioArduinoVoltajeApplication.java
+++ src/main/java/com/arduino/voltaje/ServicioArduinoVoltajeApplication.java
@@ -1,14 +1,13 @@
 package com.arduino.voltaje;
 
-import java.util.TimeZone;
-
-import javax.annotation.PostConstruct;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -24,9 +23,14 @@ public class ServicioArduinoVoltajeApplication {
 		SpringApplication.run(ServicioArduinoVoltajeApplication.class, args);
 	}
 
-    @Scheduled(fixedDelay = 120000, initialDelay = 500)
+    @Scheduled(fixedDelay = 60*1000*15, initialDelay = 500)
     public void herokuNotIdle() {
-	System.out.println(" Heroku not idle execution ");
+	System.out.println(" INIT - Heroku not idle execution ");
+	RestTemplate restTemplate = new RestTemplate();
+	ResponseEntity<String> response = restTemplate.exchange("https://arduino-voltaje-heroku.herokuapp.com/help", HttpMethod.GET, null, String.class);
+//	ResponseEntity<String> response2 = restTemplate.exchange("https://eureka-server-heroku.herokuapp.com/", HttpMethod.GET, null, String.class);
+	System.out.println(" FINISH - Heroku not idle execution: " + response);
+//	System.out.println(" FINISH - Heroku not idle execution: " + response2);
 
     }
 }
